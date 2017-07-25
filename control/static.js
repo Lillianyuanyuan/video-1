0661afe26d5d8359c15fa5cf9ad9de31f51da65d
swnb <1786718956@qq.com>
2017-07-25 17:05:12 +0800

    添加注释和修改

--- control/static.js
+++ control/static.js
@@ -1,5 +1,5 @@
 /*
- *    静态路由
+ *    静态文件路由
  */
 const path = require('path');
 const fs = require('fs');
@@ -11,6 +11,7 @@ const func = async(soc, next) => {
     p = path.normalize(p);
     p = path.join(__dirname, '../', p);
     console.log(p, ': static.js 8');
+    //回调用promise来写
     const promise = new Promise((resolve) => {
         fs.exists(p, (bool) => {
             resolve(bool);
@@ -21,7 +22,7 @@ const func = async(soc, next) => {
         soc.response.type = mime.lookup(p);
         soc.response.status = 200;
         soc.response.body = fs.readFileSync(p, 'utf-8');
-    } else if (!swi) {
+    } else {
         soc.redirect('/404');
     }
 }
