95e91d9bb76dac62c1e527b1df597e523098e2b8
swnb <1786718956@qq.com>
2017-07-25 17:04:40 +0800

    cookie模块

---
+++ control/signIn.js
@@ -0,0 +1,16 @@
+/*
+ *   cookie 路由
+ */
+const func = async(soc, next) => {
+    const name = soc.body.username;
+    console.log(name, ': signIn.js 3');
+    ctx.cookies.set('username', name, {
+        expires: new Date(Date.now() + 12000)
+    });
+    ctx.redirect('/index.swnb');
+}
+module.exports = {
+    pathName: '/signin.swnb',
+    method: 'post',
+    func,
+}
