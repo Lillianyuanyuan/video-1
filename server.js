6366b30aa51a86ede558357273da669dfbf3f0bc
swnb <1786718956@qq.com>
2017-07-26 00:35:57 +0800

    用promise替代events

--- server.js
+++ server.js
@@ -1,42 +1,52 @@
 // created by swnb （小白不黑）
 const fs = require('fs');
 const path = require('path');
-const evEmit = require('events');
-const e = new evEmit();
 const Koa = require('koa');
 const bodyparser = require('koa-bodyparser')();
 const router = require('koa-router')();
 const app = new Koa();
 app.use(bodyparser);
-fs.readdir('./control/', (err, files) => {
-    if (!err) {
-        for (file of files) {
-            if (file.endsWith('.js')) {
-                const p = path.resolve('./control', file);
-                console.log('router:', file);
-                const { pathName, method, func } = require(p);
-                if (method === 'get') {
-                    router.get(pathName, func);
-                } else {
-                    router.post(pathName, func);
+const main = async() => {
+    const router_flag = new Promise((resolve) => {
+        fs.readdir('./control/', (err, files) => {
+            if (!err) {
+                for (file of files) {
+                    if (file.endsWith('.js')) {
+                        const p = path.resolve('./control', file);
+                        console.log('router:', file);
+                        const { pathName, method, func } = require(p);
+                        if (method === 'get') {
+                            router.get(pathName, func);
+                        } else if (method === 'post') {
+                            router.post(pathName, func);
+                        } else {
+                            console.log('something wrong about router');
+                        }
+                    }
                 }
+                //注册路由
+                app.use(router.routes());
+                resolve('router register');
+            } else {
+                console.log(err);
             }
-        }
-        app.use(router.routes());
-        e.emit('router_end', 'router_finish');
-    } else {
-        console.log(err);
+        });
+    });
+
+    const swi = await router_flag;
+    console.log(swi);
+    //路由注册完成后
+    if (swi) {
+        app.use(async(ctx, next) => {
+            if (ctx.request.path === '/') {
+                ctx.redirect('/sign.swnb');
+            } else {
+                ctx.redirect('/404');
+            }
+            await next();
+        });
+        const { port } = require('./config');
+        app.listen(port);
     }
-});
-e.on('router_end', (message) => {
-    console.log(message);
-    app.use(async(ctx, next) => {
-        if (ctx.request.path === '/') {
-            ctx.redirect('/sign.swnb');
-        } else {
-            ctx.redirect('/404');
-        }
-        await next();
-    })
-    app.listen(8080);
-})
+};
+main();
