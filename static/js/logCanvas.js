b8c3ce9094adbb56339a4d6a631785dfc79485aa
swnb <1786718956@qq.com>
2017-07-27 01:33:18 +0800

    添加log的canvas文件

---
+++ static/js/logCanvas.js
@@ -0,0 +1,13 @@
+(() => {
+    let canvas = document.querySelector('canvas');
+    let [w, h] = [window.innerWidth, window.innerHeight];
+    [canvas.width, canvas.height] = [w, h];
+    // window.addEventListener('resize', () => {
+    //     [canvas.width, canvas.height] = [w, h] = [window.innerWidth, window.innerHeight];
+    // });
+    class point {
+        init() {
+
+        }
+    }
+})()
