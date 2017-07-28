0debac590b59d536453fb742e5838a27b1ad0ccd
swnb <1786718956@qq.com>
2017-07-28 22:57:47 +0800

    add root

--- config.js
+++ config.js
@@ -5,4 +5,5 @@ module.exports = {
     ETag: undefined, //因为选择了last-modified，所以就放弃了etag
     port: 8080, //端口号
     protect_static_file: ['js/save.js', 'js/test.js'], //需要保护的静态文件，不会被直接引用，会有权限认证
+    root: 'swnb', //root 用户
 }
