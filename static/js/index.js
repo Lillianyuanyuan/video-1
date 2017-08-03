ea14f8c452c79c6216dc4302f95d5a9ca5024ffb
swnb <1786718956@qq.com>
2017-08-04 03:09:17 +0800

    index file change

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,49 @@
 (() => {
-    //不允许托选
-    document.onselectstart = () => false;
-})()
+    let button = document.querySelector('.icon');
+    let li = document.querySelectorAll('.list li');
+    let swi = true;
+    let count = 1;
+    li = Array.from(li);
+    for (let i of li) {
+        i.classList.add('transformleft');
+    }
+    button.addEventListener('click', () => {
+        if (swi) {
+            for (let i of li) {
+                if (count) {
+                    i.classList.add('transition');
+                }
+                i.classList.remove('transformleft');
+            }
+        } else {
+            for (let i of li) {
+                i.classList.add('transformleft');
+            }
+        }
+        swi = !swi;
+        count = null;
+    })
+})();
+(() => {
+    let x = 20;
+    let add = true;
+    let path = document.querySelector('path');
+
+    function render() {
+        let d = 'M0,0 Q0,30 0,20 Q' + x + ',100 20,70 T40,70 T60,70 T80,70 T100,20 Q100,30 150,0';
+        path.setAttribute('d', d);
+        if (x < 0) {
+            add = true;
+        }
+        if (x > 15) {
+            add = false;
+        }
+        if (add) {
+            x += 0.2;
+        } else {
+            x -= 0.2;
+        }
+        requestAnimationFrame(render);
+    };
+    render();
+})();
