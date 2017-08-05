165639a5cd1926b98c789bff110541ca72193b08
swnb <1786718956@qq.com>
2017-08-06 00:44:39 +0800

    save change

---
+++ static/js/upload.js
@@ -0,0 +1,27 @@
+  (() => {
+      let input = document.querySelector('#file');
+      file.addEventListener('change', function() {
+
+      });
+      let button = document.querySelector('.button');
+      button.addEventListener('click', () => {
+          if (!input.files[0]) {
+              return void alert('选择文件');
+          }
+
+          let formdata = new FormData();
+          formdata.append(input.files[0].name, input.files[0]);
+          let xhr = new XMLHttpRequest();
+          xhr.open('post', '/upload', true);
+          xhr.upload.onloadstart = () => {
+              window.onbeforeunload = (e) => {
+                  return void alert('uploading');
+              }
+          }
+          xhr.upload.onprogress = (e) => {
+              e = e || window.event;
+              let percent = e.loaded / e.total;
+          }
+          xhr.send(formdata);
+      })
+  })()
