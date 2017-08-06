439e4fd90d489906a54902551f2558104ff93031
swnb <1786718956@qq.com>
2017-08-07 04:25:46 +0800

    这是我写的最头疼的动画之一,不是因为困难,而是因为需要不断的重构

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,26 +1,57 @@
-  (() => {
-      let input = document.querySelector('#file');
-      file.addEventListener('change', function() {
+/* upload file*/
+let log = console.log;
 
-      });
-      let button = document.querySelector('.button');
-      button.addEventListener('click', () => {
-          if (!input.files[0]) {
-              return void alert('选择文件');
-          };
-          let formdata = new FormData();
-          formdata.append(input.files[0].name, input.files[0]);
-          let xhr = new XMLHttpRequest();
-          xhr.open('post', '/upload', true);
-          xhr.upload.onloadstart = () => {
-              window.onbeforeunload = (e) => {
-                  return void alert('uploading');
-              }
-          }
-          xhr.upload.onprogress = (e) => {
-              e = e || window.event;
-              let percent = e.loaded / e.total;
-          }
-          xhr.send(formdata);
-      })
-  })()
+function $(str) {
+    let a;
+    if (typeof str === 'string') {
+        a = document.querySelector(str);
+    } else {
+        a = str;
+    }
+    let b = function() {
+        this.obj = a;
+    }
+    b.prototype = {
+        ev(str, fn) {
+            this.obj.addEventListener(str, fn);
+        },
+        s(obj) {
+            for (let x in obj) {
+                this.obj.style[x] = obj[x];
+            }
+        },
+        html(h) {
+            this.obj.innerHTML = h.innerHTML;
+        },
+        setA(attr, value) {
+            this.obj.setAttribute(attr, value);
+        }
+    }
+    return new b();
+}
+(() => {
+    // let input = document.querySelector('#file');
+    // file.addEventListener('change', function() {
+
+    // });
+    // let button = document.querySelector('.button');
+    // button.addEventListener('click', () => {
+    //     if (!input.files[0]) {
+    //         return void alert('选择文件');
+    //     };
+    //     let formdata = new FormData();
+    //     formdata.append(input.files[0].name, input.files[0]);
+    //     let xhr = new XMLHttpRequest();
+    //     xhr.open('post', '/upload', true);
+    //     xhr.upload.onloadstart = () => {
+    //         window.onbeforeunload = (e) => {
+    //             return void alert('uploading');
+    //         }
+    //     }
+    //     xhr.upload.onprogress = (e) => {
+    //         e = e || window.event;
+    //         let percent = e.loaded / e.total;
+    //     }
+    //     xhr.send(formdata);
+    // })
+})();
