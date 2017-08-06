5bcca0fbb6bd4283f777a6d13876c1b835577364
swnb <1786718956@qq.com>
2017-08-07 05:04:05 +0800

    upload

--- static/js/upload.js
+++ static/js/upload.js
@@ -55,3 +55,32 @@ function $(str) {
     //     xhr.send(formdata);
     // })
 })();
+
+(() => {
+    $('#audio').ev('click', () => {
+        fetch('/media/audio').then((data) => data.json()).then((data) => {
+            let ul = document.createElement('ul');
+            for (let x in data) {
+                let name = x;
+                let li = document.createElement('li');
+                li.setAttribute('data-id', data[x].id);
+                li.innerText = name;
+                ul.appendChild(li);
+            }
+            $("#audio_l").html(ul);
+        });
+    });
+    $('#video').ev('click', () => {
+        fetch('/media/video').then((data) => data.json()).then((data) => {
+            let ul = document.createElement('ul');
+            for (let x in data) {
+                let name = x;
+                let li = document.createElement('li');
+                li.setAttribute('data-id', data[x].id);
+                li.innerText = name;
+                ul.appendChild(li);
+            }
+            $("#video_l").html(ul);
+        });
+    });
+})();
