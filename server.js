375deeaaabeeb4c818772be9516a9dc36645b9d7
swnb <1786718956@qq.com>
2017-09-14 17:33:59 +0800

    替换称为 async 函数

--- server.js
+++ server.js
@@ -5,16 +5,19 @@ const Koa = require('koa');
 const { port, uploadDir } = require('./config');
 const koabody = require('koa-body');
 const router = require('koa-router')();
+const websocket = require('./controllers/websocket/ws');
 const app = new Koa();
 
-function main() {
-    const routerRigister = new Promise(resolve => {
+async function main() {
+    const message = await new Promise(resolve => {
         fs.readdir('./controllers/', (err, files) => {
             if (!err) {
-                for (file of files) {
+                for (let file of files) {
+                    //确认是js文件
                     if (file.endsWith('.js')) {
                         const p = path.join(__dirname, 'controllers', file);
                         console.log('router:', file);
+                        //获取文件和方法
                         const { pathName, get, post } = require(p);
                         //注册路由
                         post
@@ -38,22 +41,26 @@ function main() {
                 resolve('router register finish');
             } else {
                 console.log(err);
+                process.exit(0);
             }
         });
     });
     //路由注册完成后
-    routerRigister.then(message => {
-        console.log(message);
-        app.use(async (ctx, next) => {
-            if (ctx.request.path === '/') {
-                ctx.redirect('/sign.swnb');
-            } else {
-                ctx.redirect('/404');
-            }
-            await next();
-        });
-        app.listen(port);
+    console.log(message);
+    //处理错误的路由
+    app.use(async (ctx, next) => {
+        if (ctx.request.path === '/') {
+            ctx.redirect('/sign.swnb');
+        } else {
+            ctx.redirect('/404');
+        }
+        await next();
     });
+    // 获取server
+    const server = app.listen(port);
+    //交给ws使用，开启websocket
+    websocket(server);
 }
 
+//开始运行
 main();
