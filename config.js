b5068ab73db80624e75e3d97e9128e3e62ab57be
swnb <1786718956@qq.com>
2017-09-15 00:17:10 +0800

    最大在线人数

--- config.js
+++ config.js
@@ -7,7 +7,6 @@ module.exports = {
     port: 8080, //端口号
     protect_static_file: ['js/save.js', 'js/test.js'], //需要保护的静态文件，不会被直接访问，会有权限认证
     root: 'swnb', //root 用户
-    cookies_expires: 60 * 60000, // cookies过期时间为一个小时1000是一秒,
-    uploadDir: path.join(__dirname, '/static/mediaData/audio/'),
-    maxUserNumber: 1000
+    cookies_expires: 60 * 60000, // cookies过期时间为一个小时,1000是一秒,
+    uploadDir: path.join(__dirname, '/static/mediaData/audio/')
 };
