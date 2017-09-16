02618f12ffb927ed7d8e0a733246b679b29022e4
swnb <1786718956@qq.com>
2017-09-16 16:33:32 +0800

    使用es6的trim()

--- api.js/getCookies.js
+++ api.js/getCookies.js
@@ -1,13 +1,10 @@
-function trim(str) {
-    return str.replace(/(^\s+)|(\s+$)/, '');
-}
 module.exports = cookie => {
     if (/;/.test(cookie)) {
         let cookies = {};
         let arr = cookie.split(';');
         arr = arr.map(ele => ele.split('='));
         for (z of arr) {
-            cookies[trim(z[0])] = trim(z[1]);
+            cookies[z[0].trim()] = z[1].trim();
         }
         return cookies;
     } else {
