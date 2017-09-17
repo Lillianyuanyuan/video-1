b812ade85c5f0b96fe81ef1c3f8a34362be1c96f
swnb <1786718956@qq.com>
2017-09-17 14:59:55 +0800

    一个测试

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
     ETag: undefined, //因为选择了last-modified，所以就放弃了etag
 
-    port: 8000, //端口号
+    port: 8080, //端口号
 
     protect_static_file: ['js/save.js', 'js/test.js'], //需要保护的静态文件，不会被直接访问，会有权限认证
 
