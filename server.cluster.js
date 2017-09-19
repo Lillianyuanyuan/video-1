d20644da6f0d5c005d5e1bd58c7fb0dc91f8c50f
swnb <1786718956@qq.com>
2017-09-19 23:27:38 +0800

    add cluster

--- server.cluster.js
+++ server.cluster.js
@@ -1,4 +1,5 @@
 const cluster = require('cluster');
+const { spawn } = require('child_process');
 if (cluster.isMaster) {
     cluster.fork();
     cluster.on('exit', (worker, code, signal) => {
@@ -7,4 +8,15 @@ if (cluster.isMaster) {
         console.log(`restart it `);
     });
 } else if (cluster.isWorker) {
+    let server = spawn('node', ['server.js']);
+
+    // 捕获标准输出并将其打印到控制台
+    server.stdout.on('data', data => {
+        console.log(data);
+    });
+
+    // 捕获标准错误输出并将其打印到控制台
+    server.stderr.on('data', data => {
+        console.log('错误\n' + data);
+    });
 }
